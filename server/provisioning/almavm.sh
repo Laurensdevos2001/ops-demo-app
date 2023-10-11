4ea4f05d2cf1c6fafcf28e13bdd46052c32bf2df
Laurens DE VOS <laurens.devos@student.hogent.be>
2023-10-11 18:27:53 +0200

    vm aangemaakt

---
+++ server/provisioning/almavm.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# Bash settings
+#------------------------------------------------------------------------------
+
+# Enable "Bash strict mode"
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't mask errors in piped commands
+
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
