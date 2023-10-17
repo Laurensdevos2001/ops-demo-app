c859c39ed3d7b261d7f49129f9ffdd1af5b71111
Laurens DE VOS <laurens.devos@student.hogent.be>
2023-10-17 11:19:52 +0200

    jenkins script

--- server/provisioning/almavm.sh
+++ server/provisioning/almavm.sh
@@ -12,3 +12,26 @@ set -o pipefail  # don't mask errors in piped commands
 #------------------------------------------------------------------------------
 # Variables
 #------------------------------------------------------------------------------
+
+install_packages() {
+    dnf update -y && sudo dnf upgrade -y
+    dnf install -y nano
+    dnf install -y wget
+    dnf install java-11-openjdk java-11-openjdk-devel -y
+}
+
+install_jenkins() {
+
+    # get jenkins repo en GPG key
+    wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+    # install jenkins
+    dnf install jenkins -y
+
+    # start en enable
+    systemctl start jenkins && sudo systemctl enable jenkins
+}
+
+install_packages
+install_jenkins
